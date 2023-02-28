82f438c3bc2d94b332639668a02b426bf9c83f4b
baugod <bautistagodoy060@gmail.com>
2023-02-28 15:41:17 -0300

    Base de Backend

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import dataBase from "./src/config/db.js";
+import serverHttp from "./src/config/server.js";
+import dotenv from "dotenv";
+// importamos la info de la api
+// import apiInfo from "./src/helpers/apiInfo.js"
+
+dotenv.config();
+
+const index = async()=> {
+  await dataBase.sync({force:true})
+//   await apiInfo()
+  serverHttp.listen(3001,()=>{
+    console.log('Listening on port 3001')
+  })
+};
+
+index();
---
+++ api/src/config/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+//importo las rutas
+import post from '../routes/post.Coment';
+import products from '../routes/products.Router';
+
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use(cors());
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+server.use('/products', products);
+server.use('/comentaries', post); 
+
+// Error catching endware.
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+export default server;
