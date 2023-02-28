4b87ab3aa26a63d3bc67d20bda27fdcce34e796c
baugod <bautistagodoy060@gmail.com>
2023-02-28 15:55:11 -0300

    Backend

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import dataBase from "./src/config/db.js";
 import serverHttp from "./src/config/server.js";
 import dotenv from "dotenv";
 // importamos la info de la api
-// import apiInfo from "./src/helpers/apiInfo.js"
+//import apiInfo from "./src/helpers/apiInfo.js"
 
 dotenv.config();
 
