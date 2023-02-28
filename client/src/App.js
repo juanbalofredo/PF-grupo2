b23d89d6e0b19f1bfda34e03d942803fbc220488
NicoGalarce1 <nicolasgalarce18@gmail.com>
2023-02-28 21:37:04 +0100

    Routes home y landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import "./styles/styles.css"
+import "./styles/styles.css";
+import Landing from "./views/Landing";
+import Home from "./views/Home";
+import { Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      hola mundo
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
