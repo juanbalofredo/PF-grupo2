2630700a6c53687bf375622f4a3648b21e08986d
Terry <danyel.martel@gmail.com>
2023-03-01 17:38:43 -0500

    Co-authored-by: Alex Huaitiao Añasco <alexaniasco@users.noreply.github.com>

--- api/src/helpers/apiInfo.js
+++ api/src/helpers/apiInfo.js
@@ -3,52 +3,24 @@ import Products from "../models/products.js";
 import products from "../prueba (1).js";
 
 const apiInfo = async () => {
-<<<<<<< HEAD
-    const produc = products.map(async (p) => {
-        return await Products.create({
-            name: p.name,
-            brand: p.brand,
-            price: p.price,
-            unit: p.unity,
-            category: p.category,
-            description: p.description,
-            supermarket: p.supermarket,
-            image: p.image
-        })
-    })
-    produc.forEach(e => {
-        e
-            .then(info => console.log(info.dataValues))
-    })
-    console.log("Esto es produc ==>", produc)
-    return produc;
-=======
-  const resultados = [];
-  products.map((p) => {
-    Products.create({
-      name: p.name,
-      brand: p.brand,
-      price: p.price,
-      unit: p.unity,
-      category: p.category,
-      description: p.description,
-      supermarket: p.supermarket,
-      image: p.image,
-    });
-    resultados.push({
-      name: p.name,
-      brand: p.brand,
-      price: p.price,
-      unit: p.unity,
-      category: p.category,
-      description: p.description,
-      supermarket: p.supermarket,
-      image: p.image,
-    });
-  });
+
+    for (let i = 0; i < products.length; i++) {
+        await Products.create({
+            name: products[i].name,
+            brand: products[i].brand,
+            price: products[i].price,
+            unit: products[i].unity,
+            category: products[i].category,
+            description: products[i].description,
+            supermarket: products[i].supermarket,
+            image: products[i].image,
+        });
+    }
+
+    const todosLosProductos = await Products.findAll()
 
 
-  return resultados;
->>>>>>> f3cfad38f418cc9ce66d42e67623641bf9700028
+    return todosLosProductos;
+
 };
 export default apiInfo;
