f3cfad38f418cc9ce66d42e67623641bf9700028
alexaniasco <alexaniasco@outlook.com>
2023-03-01 19:12:36 -0300

    Trae los objetos a la primera

--- api/src/controllers/allProducts.js
+++ api/src/controllers/allProducts.js
@@ -2,10 +2,14 @@ import apiInfo from "../helpers/apiInfo.js"
 import Products from "../models/products.js";
 // asdasd
 export default async function getProducts(req, res){
+    const response2 = await Products.findAll()
     try{
-        apiInfo();
-        const response2 = await Products.findAll()
-        return res.status(200).json(response2);
+        if(response2.length == 0){
+        apiInfo().then(r=> res.status(200).json(r) );
+        }
+        else return res.status(200).json(response2);
+      
+       
     } catch(error) {
         return res.status(400).json({err: error.message});
     }
--- api/src/helpers/apiInfo.js
+++ api/src/helpers/apiInfo.js
@@ -1,21 +1,33 @@
 import Products from "../models/products.js";
-//products es el json de todos los productos 
+//products es el json de todos los productos
 import products from "../prueba (1).js";
 
 const apiInfo = async () => {
-    const produc = products.map(p => {
-        Products.create({
-            name: p.name,
-            brand: p.brand,
-            price: p.price,
-            unit: p.unity,
-            category: p.category,
-            description: p.description,
-            supermarket: p.supermarket,
-            image: p.image
-        })
-    })
+  const resultados = [];
+  products.map((p) => {
+    Products.create({
+      name: p.name,
+      brand: p.brand,
+      price: p.price,
+      unit: p.unity,
+      category: p.category,
+      description: p.description,
+      supermarket: p.supermarket,
+      image: p.image,
+    });
+    resultados.push({
+      name: p.name,
+      brand: p.brand,
+      price: p.price,
+      unit: p.unity,
+      category: p.category,
+      description: p.description,
+      supermarket: p.supermarket,
+      image: p.image,
+    });
+  });
 
-    return produc;
+
+  return resultados;
 };
 export default apiInfo;
