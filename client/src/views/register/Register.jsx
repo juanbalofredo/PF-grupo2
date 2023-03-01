2bc6a01895e1d410c8de0eb915f568cb3e38f7f5
juanbalofredo <juanlofredocs6sj@gmail.com>
2023-03-01 10:55:19 -0300

    juancito

---
+++ client/src/views/register/Register.jsx
@@ -0,0 +1,18 @@
+import "./register.css"
+import { useState } from "react"
+
+
+const Register = () => {
+
+
+
+
+
+
+return(
+    <div className="register-container">
+
+    </div>
+)}
+
+export default Register
