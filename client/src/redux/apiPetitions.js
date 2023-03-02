2e21d894f28c1d6071ce6081fdb5d192ab232d8d
NicoGalarce1 <nicolasgalarce18@gmail.com>
2023-03-02 01:36:36 +0100

    Productos en el home y Card

--- client/src/redux/apiPetitions.js
+++ client/src/redux/apiPetitions.js
@@ -3,7 +3,8 @@ import { allProducts } from "./slice";
 
 export async function getAllProducts(dispatch) {
     try {
-      const peticion = await axios.get("http://localhost:3001/");
+      const peticion = await axios.get("http://localhost:3001/products");
+      console.log(peticion.data);
       dispatch(allProducts(peticion?.data));
     } catch (error) {
       return error.response;
--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,17 +1,21 @@
 import './home.css'
 import NavBar from '../../components/assets/NavBar'
 import { useSelector } from "react-redux";
+import Card from '../Card/Card';
+
 const Home =()=>{
-const state = useSelector((state) => state.productsBackup);    
-console.log(state);  
+const state = useSelector((state) => state.bolsillo);
 return(
     <>
     <div className="Navbar-Home">
     <NavBar/>
     </div>
     <div className='Home-container'>
-        <div className="Home-container-products">
-        </div>
+    <div className="Home-container-products">
+    {state.productsBackup.map((p)=>(
+        <Card key={p.id} product={p} />
+        ))}  
+    </div>
     </div>
     </>
 )
