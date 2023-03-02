80fe0b6877a0ba934fc31be1100570fedd59b33d
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-02 18:48:42 -0300

    home

--- client/src/views/Card/Card.jsx
+++ client/src/views/Card/Card.jsx
@@ -1,8 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./card.css";
-import img from "../Card/produc.jpg";
 export default function Card({ product }) {
-  const { name, brand, price, image } = product;
+  const { name, brand, image } = product;
 
     const navigate = useNavigate();
     const click = () => {
@@ -16,7 +15,6 @@ export default function Card({ product }) {
           <p className="text-title">{name}</p>
             <img className="image" src={image} alt={name} key={name}></img>
             <p className="text-brand">{brand}</p>
-            <p className="text-price">${price}</p>
           </div>
           <button className="card-button">More info</button>
         </div>
