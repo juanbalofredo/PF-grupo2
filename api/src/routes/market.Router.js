b76c1951bf2443374cb88e42c7f3786c59a3819b
baugod <bautistagodoy060@gmail.com>
2023-03-03 11:55:19 -0300

    Ruta delete market

--- api/src/routes/market.Router.js
+++ api/src/routes/market.Router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllMarket, getById} from "../controllers/market.controller.js"
+import { getAllMarket, getById, deleteMarket} from "../controllers/market.controller.js"
 
 const market = Router();
 
@@ -8,6 +8,6 @@ market.get("/id/:id", getById);
 
 market.put("/putmarket/:id", )
 
-market.delete("/deletemarket/:id", )
+market.delete("/deletemarket/:id", deleteMarket );
 
 export default market;
