fceaa188e3eaecc387b9e8d141476a1f1f66b29d
pacodinucci <franciscoldinucci@gmail.com>
2023-03-03 14:22:30 -0300

    'problema'

---
+++ api/src/controllers/reviews.controller.js
@@ -0,0 +1,22 @@
+import Reviews from "../models/comments.js";
+
+export default async function getReviews(req, res){
+    const allReviews = await Reviews.findAll();
+    try{
+        if(allReviews.length == 0){
+            return res.status(400).send("No hay reviews")
+        } else return res.status(200).json(allReviews);
+    }
+    catch(error){
+        return res.status(400).json({ err: error.message });
+    }
+}
+
+export async function postReview(req, res){
+    
+}
+
+
+
+
+//agregando comentarios
