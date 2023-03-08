0846fe3c9f5f56395c56e8ca741bf5004826dba4
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-08 02:27:08 -0300

    primero de prueba

--- client/src/components/filtro/Filtro.jsx
+++ client/src/components/filtro/Filtro.jsx
@@ -5,93 +5,93 @@ import { getCategoryParams, rsetFilters } from "../../redux/apiPetitions";
 import "../filtro/filtro.css";
 
 const Filtro = () => {
-  const state = useSelector((state) => state.bolsilloFeliz);
-  const allCategories = [
-    ...new Set(state.productsBackup.map((a) => a.category)),
-  ].sort();
-  const todasMarcas = state.productsBackup.map((a) => a.brand); //con repetidos
-  const dispatch = useDispatch();
-  const duplicatesArray = [];
-  const seenElements = {};
-  const valor = "ASC";
-  const stateSupermarket = useSelector((state) => state.bolsilloFeliz.brand);
-  const stateCategory = useSelector((state) => state.bolsilloFeliz.category);
-  todasMarcas.forEach((element) => {
-    if (seenElements[element]) {
-      duplicatesArray.push(element);
-    } else {
-      seenElements[element] = true;
-    }
-  });
-  const sinNorepetidos = [...new Set(duplicatesArray)].sort();
+  // const state = useSelector((state) => state.bolsilloFeliz);
+  // const allCategories = [
+  //   ...new Set(state.productsBackup.map((a) => a.category)),
+  // ].sort();
+  // const todasMarcas = state.productsBackup.map((a) => a.brand); //con repetidos
+  // const dispatch = useDispatch();
+  // const duplicatesArray = [];
+  // const seenElements = {};
+  // const valor = "ASC";
+  // const stateSupermarket = useSelector((state) => state.bolsilloFeliz.brand);
+  // const stateCategory = useSelector((state) => state.bolsilloFeliz.category);
+  // todasMarcas.forEach((element) => {
+  //   if (seenElements[element]) {
+  //     duplicatesArray.push(element);
+  //   } else {
+  //     seenElements[element] = true;
+  //   }
+  // });
+  // const sinNorepetidos = [...new Set(duplicatesArray)].sort();
 
-  async function handleFilterCategory(e) {
-    getCategoryParams(dispatch, e, stateSupermarket, valor);
-  }
+  // async function handleFilterCategory(e) {
+  //   getCategoryParams(dispatch, e, stateSupermarket, valor);
+  // }
 
-  async function handleFilterBrand(e) {
-    getCategoryParams(dispatch, stateCategory, e, valor);
-  }
+  // async function handleFilterBrand(e) {
+  //   getCategoryParams(dispatch, stateCategory, e, valor);
+  // }
 
-  async function handleFilterReset() {
+  // async function handleFilterReset() {
 
-    dispatch(rsetFilters(dispatch));
-  }
+  //   dispatch(rsetFilters(dispatch));
+  // }
 
-  return (
-    <>
-      <div className="filtaa-container">
-        <div className="filt-tipo">
-          <h3
-            name="borrar-filtros"
-            onClick={(e) => handleFilterReset()}
-            id="borrar-filtros"
-          >
-            Borrar filtros
-          </h3>
-          <h3 name="filtPorTipo" id="filtPorTipo">
-            Categoria
-          </h3>
-          <label
-            onClick={(e) => handleFilterReset()}
-            value="All"
-            key={todasMarcas}
-          >
-            Todas
-          </label>
-          {allCategories.map((categoria, index) => (
-            <div key={index}>
-              <label
-                onClick={(e) => handleFilterCategory(categoria)}
-                value={categoria}
-              >
-                {categoria}
-              </label>
-            </div>
-          ))}
-        </div>
-        <br />
-        <div className="filt-relleno">
-          <h3 name="filtPorRelleno" id="filtPorRelleno">
-            Marca
-          </h3>
-          <label onClick={(e) => handleFilterReset()} value="">
-            Todas
-          </label>
-          {sinNorepetidos.map((categoria, index) => (
-            <div key={index}>
-              <label
-                onClick={(e) => handleFilterBrand(categoria)}
-                value={categoria}
-              >
-                {categoria}
-              </label>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="filtaa-container">
+  //       <div className="filt-tipo">
+  //         <h3
+  //           name="borrar-filtros"
+  //           onClick={(e) => handleFilterReset()}
+  //           id="borrar-filtros"
+  //         >
+  //           Borrar filtros
+  //         </h3>
+  //         <h3 name="filtPorTipo" id="filtPorTipo">
+  //           Categoria
+  //         </h3>
+  //         <label
+  //           onClick={(e) => handleFilterReset()}
+  //           value="All"
+  //           key={todasMarcas}
+  //         >
+  //           Todas
+  //         </label>
+  //         {allCategories.map((categoria, index) => (
+  //           <div key={index}>
+  //             <label
+  //               onClick={(e) => handleFilterCategory(categoria)}
+  //               value={categoria}
+  //             >
+  //               {categoria}
+  //             </label>
+  //           </div>
+  //         ))}
+  //       </div>
+  //       <br />
+  //       <div className="filt-relleno">
+  //         <h3 name="filtPorRelleno" id="filtPorRelleno">
+  //           Marca
+  //         </h3>
+  //         <label onClick={(e) => handleFilterReset()} value="">
+  //           Todas
+  //         </label>
+  //         {sinNorepetidos.map((categoria, index) => (
+  //           <div key={index}>
+  //             <label
+  //               onClick={(e) => handleFilterBrand(categoria)}
+  //               value={categoria}
+  //             >
+  //               {categoria}
+  //             </label>
+  //           </div>
+  //         ))}
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default Filtro;
