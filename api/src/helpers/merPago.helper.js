1b10f8d0d9d84fcacf5c704c02864ebd31362634
baugod <bautistagodoy060@gmail.com>
2023-03-09 10:10:15 -0300

    Mp funcionando

--- api/src/helpers/merPago.helper.js
+++ api/src/helpers/merPago.helper.js
@@ -1,23 +1,31 @@
 import axios from "axios";
 
-export async function subscriptionHelper(req, res){
+class PaymentService {
+ 
+  async createSubscription() {
     const url = "https://api.mercadopago.com/preapproval";
 
-    let preference = {
-        reason: "Suscripcion de supermercados",
-		auto_recurring: {
-            frequency: 1,
-            frequency_type: "months",
-            transaction_amount: 500,
-            currency_id: "ARS"
-        },
-        back_url: "https://yoursite.com.ar/succes",
-        payer_email: "test_user_55610397@testuser.com"
-	};
+    const body = {
+      reason: "Suscripción de ejemplo",
+      auto_recurring: {
+        frequency: 1,
+        frequency_type: "months",
+        transaction_amount: 10,
+        currency_id: "ARS"
+      },
+      back_url: "https://google.com.ar",
+      payer_email: "test_user_46945293@testuser.com"
+    };
 
-    const subscription = await axios.post(url, preference, {
-        "Content-Type":"application/json",
+    const subscription = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.MP_TOKEN}`
-    })
+      }
+    });
+
     return subscription.data;
+  }
 }
+
+export default PaymentService;
