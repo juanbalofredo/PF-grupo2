4b0b84c91beaec79d983407e87d11d6540dd5779
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-09 18:13:33 -0300

    wpp

--- client/src/views/footer/Footer.jsx
+++ client/src/views/footer/Footer.jsx
@@ -4,9 +4,15 @@ import fb from "../../assets/facebook.svg"
 import twitter from "../../assets/twitter.svg"
 import linkedin from "../../assets/linkedin.svg"
 import instagram from "../../assets/instagram.svg"
+import { useSelector } from "react-redux";
+
 
 
 export const Footer = () => {
+
+  const state = useSelector((state) => state.bolsilloPersist);
+  const logeado = state.user; 
+
   return (
     <div className="footer">
       <div className="sb__footersection__padding">
@@ -27,7 +33,13 @@ export const Footer = () => {
                 <p><img src={instagram} alt="" /></p>
             </div>
         </div>
-      </div>
+      </div>{  logeado ?
+      <div className="footer-tenes-c">
+        <h4>Tenes un supermercado y queres agregar tus productos a la pagina?</h4> 
+          <a href=
+           {`https://wa.me/541132695097?text=Hola quiero ser mercader en tu pagina de Bolsillo Feliz, email${state.email}`} 
+                  target="_blank" rel="noreferrer"> <p>Hace click aca</p></a>
+      </div>:null}
 
       <div className="sb__footer-below">
         <div className="sb__footer-copyright">
