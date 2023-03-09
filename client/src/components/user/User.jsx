5bb7e98153b48b844c8a44e6861c4865e92e4940
Juan Bautista Lofredo <juanlofredocs6sj@gmail.com>
2023-03-09 15:12:18 -0300

    arreglos textos de dash y mercados navbar

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -1,31 +1,53 @@
 import { useSelector } from "react-redux";
 import invitado from "../../assets/invitado.png";
 import { Link } from "react-router-dom";
-import "./user.css"
+import "./user.css";
 
 export const User = () => {
-  
-
   const state = useSelector((state) => state.bolsilloPersist);
   const user = state.user;
   const image = user ? state.avatar : null;
   const rol = user ? state.type_account : null;
-  const name = user ? state.name :null
+  const name = user ? state.name : null;
   return (
     <div className="user-container">
       <div>
-        {user  ? <img className="profileImg"src={image} alt="" height="40px" width="40px" /> : <img className="profileImg"src={invitado} alt="" height="40px" width="40px" />}
+        {user ? (
+          <img
+            className="profileImg"
+            src={image}
+            alt=""
+            height="40px"
+            width="40px"
+          />
+        ) : (
+          <img
+            className="profileImg"
+            src={invitado}
+            alt=""
+            height="40px"
+            width="40px"
+          />
+        )}
       </div>
       <div className="name-container">
-      {user ? <h4 className="name">{name}</h4> : <h4 className="name"></h4> }
+        {user ? <h4 className="name">{name}</h4> : <h4 className="name"></h4>}
       </div>
-      <div className="dash-container">
-      {rol == 2 ? <h4 className="name"><Link to="/dashuser">Mi mercado</Link> </h4> : <h4 className="name"></h4> }
-      </div>
-      <div className="dash-container">
-      {rol == 3 ? <h4 className="name"><Link to="/dashuser">Lista de usuarios</Link> </h4> : <h4 className="name"></h4> }
-      </div>
-      
+      {rol === "2" ? (
+        <div className="merc-container">
+          <p className="name">
+            <Link to="/dashuser">Mi mercado</Link>{" "}
+          </p>
+        </div>
+      ) : null}
+
+      {rol === "3" ? (
+        <div className="dash-container">
+          <p className="name">
+            <Link to="/dashuser">Lista de usuarios</Link>{" "}
+          </p>{" "}
+        </div>
+      ) : null}
     </div>
   );
 };
