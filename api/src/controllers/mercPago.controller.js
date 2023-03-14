1684449691c079c8211840c4d4bc1b1361aa0948
baugod <bautistagodoy060@gmail.com>
2023-03-14 14:09:05 -0300

    Back nuevo MP

--- api/src/controllers/mercPago.controller.js
+++ api/src/controllers/mercPago.controller.js
@@ -1,23 +1,47 @@
-class PaymentController {
-  constructor(subscriptionService) {
-    this.subscriptionService = subscriptionService;
-  }
-  async getSubscriptionLink(req, res) {
-    try {
-      const subscription = await this.subscriptionService.createSubscription();
+import mercadopago from "mercadopago";
 
-      return res.status(200).json(subscription);
-    } catch (error) {
-      console.log(error);
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+	access_token: "TEST-6855367691697925-030817-8621a90b766c6fbe43a243575f2241f2-1326325722",
+});
 
-      return res
-        .status(500)
-        .json({ error: true, msg: "Failed to create subscription" });
-    }
-  }
-}
 
-export default PaymentController;
+export function payment(req, res) {
+
+	let preference = {
+		items: [
+			{
+        title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity)
+			}
+		],
+		back_urls: {
+			"success": "http://localhost:8080/feedback",
+			"failure": "http://localhost:8080/feedback",
+			"pending": "http://localhost:8080/feedback"
+		},
+		auto_return: "approved",
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+};
+
+export function feedback(req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
+};
+
 
 
 // // https://api.mercadopago.com/preapproval 
--- api/src/helpers/merPago.helper.js
+++ api/src/helpers/merPago.helper.js
@@ -1,31 +1,31 @@
-import axios from "axios";
+// import axios from "axios";
 
-class PaymentService {
+// class PaymentService {
  
-  async createSubscription() {
-    const url = "https://api.mercadopago.com/preapproval";
+//   async createSubscription() {
+//     const url = "https://api.mercadopago.com/preapproval";
 
-    const body = {
-      reason: "Suscripción de ejemplo",
-      auto_recurring: {
-        frequency: 1,
-        frequency_type: "months",
-        transaction_amount: 100,
-        currency_id: "ARS"
-      },
-      back_url: "https://google.com.ar",
-      payer_email: "test_user_46945293@testuser.com"
-    };
+//     const body = {
+//       reason: "Suscripción de ejemplo",
+//       auto_recurring: {
+//         frequency: 1,
+//         frequency_type: "months",
+//         transaction_amount: 100,
+//         currency_id: "ARS"
+//       },
+//       back_url: "https://google.com.ar",
+//       payer_email: "test_user_46945293@testuser.com"
+//     };
 
-    const subscription = await axios.post(url, body, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.MP_TOKEN}`
-      }
-    });
+//     const subscription = await axios.post(url, body, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${process.env.MP_TOKEN}`
+//       }
+//     });
 
-    return subscription.data;
-  }
-}
+//     return subscription.data;
+//   }
+// }
 
-export default PaymentService;
+// export default PaymentService;
